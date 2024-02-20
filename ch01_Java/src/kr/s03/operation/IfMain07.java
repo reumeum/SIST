9b4bf0b744bb3ac26c5654f93c3659ea9cc6a8ed
boreum <boreum1111@gmail.com>
2024-02-20 19:36:27 +0900

    2024.02.20 (4)

---
+++ ch01_Java/src/kr/s03/operation/IfMain07.java
@@ -0,0 +1,35 @@
+package kr.s03.operation;
+
+import java.util.*;
+
+public class IfMain07 {
+	public static void main(String[] args) {
+		/*
+		 * [실습] 생년월일을 입력하고 만 나이를 출력하는 프로그램을 작성하시오. 만 나이 = (현재 연도 - 태어난 연도) - (생일이 지났으면
+		 * 0, 생일이 지나지 않았으면 1) [입력 예시] 출생 연도 입력:2000 월 입력:3 입 입력:12
+		 * 
+		 * [출력 예시] 만 나이는 23
+		 */
+		// 현재 날짜 정보
+		int nowYear = 2024;
+		int nowMonth = 2;
+		int nowDate = 20;
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("출생 연도 입력: ");
+		int birthYear = input.nextInt();
+		System.out.print("월 입력: ");
+		int birthMonth = input.nextInt();
+		System.out.print("일 입력: ");
+		int birthDate = input.nextInt();
+
+		int age;
+		if (birthMonth < nowMonth || (birthMonth == nowMonth && birthDate <= nowDate)) {
+			age = nowYear - birthYear;
+		} else {
+			age = nowYear - birthYear - 1;
+		}
+
+		System.out.println("만 나이는 " + age);
+	}
+}
---
+++ ch01_Java/src/kr/s03/operation/IfMain07test.java
@@ -0,0 +1,29 @@
+package kr.s03.operation;
+
+public class IfMain07test {
+	public static void main(String[] args) {
+		/*
+		 * [실습]
+		 * 생년월일을 입력하고 만 나이를 출력하는 프로그램을 작성하시오.
+		 * 만 나이 = (현재 연도 - 태어난 연도) - (생일이 지났으면 0,
+		 *                               생일이 지나지 않았으면 1)
+		 * [입력 예시]
+		 * 출생 연도 입력:2000
+		 * 월 입력:3
+		 * 입 입력:12
+		 * 
+		 * [출력 예시]                              
+		 * 만 나이는 23                                                        
+		 */
+		//현재 날짜 정보
+		int nowYear = 2024;
+		int nowMonth = 2;
+		int nowDate = 20;
+		
+		
+	}
+}
+
+
+
+
